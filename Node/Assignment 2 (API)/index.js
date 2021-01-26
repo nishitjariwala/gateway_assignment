f3a696924666b1ef56847f33193bff6df3a67754
nishitjariwala <46419527+nishitjariwala@users.noreply.github.com>
2021-01-26 16:25:30 +0530

    Submission of Node JS Assignment 2

---
+++ Node/Assignment 2 (API)/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./queries')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+app.get('/cars', db.getCars)
+app.get('/cars/:id', db.getCarById)
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
